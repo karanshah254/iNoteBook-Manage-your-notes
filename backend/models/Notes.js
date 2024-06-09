bc1abc6eaa206edf0982864b3b39592d36e0794d
karanshah254 <karankshah2504@gmail.com>
2024-06-10 02:42:12 +0530

    Working on iNoteBook - App

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// Creating a schema for notes
 const NotesSchema = new Schema({
     title: {
         type: String,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,29 +3,38 @@ const router = express.Router();
 const User = require('../models/User');
 const { query, validationResult, body } = require('express-validator');
 
-// Create a User using: POST "/api/auth/". Doesn't require Auth
-
-router.post('/', [
+// Create a User using: POST "/api/auth/createuser". Doesn't require Auth
+router.post('/createuser', [
     body('name', 'Enter valid name').isLength({ min: 5 }),
     body('email', 'Enter valid email').isEmail(),
     body('password', 'Password must be strong and atleast 8 character').isLength({ min: 8 })
-], (req, res) => {
+], async (req, res) => {
+    // If there are errors then return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email
-    }).then(user => res.json(user))
-        .catch((error) => {
-            console.log(error);
-            res.json({ 
-                error: "Enter unique value",
-                message: error.message
-            });
-        });
+
+    // Check user with this email exists or not
+    try {
+        let user = await User.findOne({
+            email: req.body.email
+        })
+        if (user) {
+            return res.status(400).json({
+                error: "User with this email already exist"
+            })
+        }
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+        })
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
 })
 
 module.exports = router;
