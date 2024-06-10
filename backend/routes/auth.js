8956f4e71743ba39c2e2673932bd795df23565ef
karanshah254 <karankshah2504@gmail.com>
2024-06-10 15:44:14 +0530

    Working on this project

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { query, validationResult, body } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Karanisanenginerrrrr';
 
 // Create a User using: POST "/api/auth/createuser". Doesn't require Auth
 router.post('/createuser', [
@@ -15,8 +19,8 @@ router.post('/createuser', [
         return res.status(400).json({ errors: errors.array() });
     }
 
-    // Check user with this email exists or not
     try {
+        // Check user with this email exists or not
         let user = await User.findOne({
             email: req.body.email
         })
@@ -25,15 +29,68 @@ router.post('/createuser', [
                 error: "User with this email already exist"
             })
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const securedPass = await bcrypt.hash(req.body.password, salt);
+
+        //Create a new User
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: securedPass,
             email: req.body.email
-        })
-        res.json(user);
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+        // res.json(user);
+        res.json({ authToken });
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Some error occured");
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+// Authenticate a user using POST: "/api/auth/login". No login required
+router.post('/login', [
+    body('email', 'Enter valid email').isEmail(),
+    body('password', 'Password cannot be empty').exists(),
+], async (req, res) => {
+    // If there are errors then return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                error: "Enter correct login credentials"
+            });
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({
+                error: "Enter correct login credentials"
+            });
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
     }
 })
 
